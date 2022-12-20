5296ab53f9d9574c2c485367ae92b761d32a82f6
gwynbleidd111 <94562956+gwynbleidd111@users.noreply.github.com>
2022-12-20 15:18:37 +0300

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -4,7 +4,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Hw3 {
+public class Task2 {
     public static void main(String[] args) {
         System.out.println(getRandomList());
         System.out.println(removeEvenNumbers(getRandomList()));
